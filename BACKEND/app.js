b6bb7a9832e22660b179d018a9eaa37df7c3bc29
Terkihacks <raymondmunguti163@gmail.com>
2024-12-10 15:58:51 +0300

    Layed the foundation for my front end prooject

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
 dotenv.config()
+const employeeRoutes = require('./routes/employeeRoutes')
+
+//This middleware is used to parse incoming requests with URL-encoded payloads (e.g., form submissions).
+app.use(express.urlencoded({ extended: true }));
+//This middleware is used to parse incoming requests with JSON payloads (typically in application/json format).
+app.use(express.json());
+app.use('/employee',employeeRoutes)
 
 //Start the server
 app.listen(process.env.PORT || 5500,() =>{
---
+++ BACKEND/controllers/employeeController.js
@@ -0,0 +1,24 @@
+const db = require('../config/db');
+const bcrypt = require('bcryptjs');
+
+//Register a new Employee
+exports.createEmployee = async(req,res) =>{
+    try{
+    const{first_name,last_name,password,email,phone,specialization,department} = req.body;
+    const[rows] = await db.execute('SELECT * FROM employees WHERE email = ?',[email]);
+    if(rows.length > 0) return res.status(400).json({message:'Email already exists'});
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.execute(
+        `
+        INSERT INTO employees (first_name,last_name,password,email,phone,specialization,department)
+        VALUES(?,?,?,?,?,?,?)
+        `,
+        [first_name,last_name,hashedPassword,email,phone,specialization,department]
+    )
+    res.status(200).json({ message: 'Account registered successfully'});
+    }catch(error){
+        console.log(error);
+        console.error('Registration error:', error.message || error);
+        res.status(500).json({ message: 'Error registering employees account', error });
+    }
+}
