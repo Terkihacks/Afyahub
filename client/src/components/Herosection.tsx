ac2b5bc1df441a8108e9db7ea0d132e12f00c90f
Terkihacks <raymondmunguti163@gmail.com>
2024-12-18 14:32:46 +0300

    Developed types in my typescript app Created the components for my applications Created a Layout for the App components

---
+++ client/src/components/Herosection.tsx
@@ -0,0 +1,11 @@
+export default function Herosection(){
+    return(
+        <div className="flex flex-col items-center mt-6 lg:mt-20">
+            <h1 className="twxt-4xl sm:text-6xl lg:text-7xl text-centre tracking-wide">
+                Afyahub Collaborative Tool
+            </h1>
+            <span className="bg-gradient">For Medical </span>
+
+        </div>
+    );
+}
